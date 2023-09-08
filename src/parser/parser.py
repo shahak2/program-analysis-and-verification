f956f63f8a413cf32334c5494e47697bd72dc063
shahak2 <shahakdeutsch@gmail.com>
2023-09-08 21:56:34 +0300

    added parsing of program lines into tuples

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,22 +1,39 @@
+import re
 
 INVALID_PROGRAM_PATH = ""
 
 class Parser:
+    
+    def splitLabels(program_lines):
+        lines_results = dict()
+        for line_number, line in enumerate(program_lines):
+            regex_pattern = r'(L\d+)\s+(.*?)\s+(L\d+)'
+            result = re.findall(regex_pattern, line)
+            lines_results[line_number] = result[0]
+        return lines_results
     
     
     def __init__(self, program_path = INVALID_PROGRAM_PATH):
         assert program_path != INVALID_PROGRAM_PATH, f"Invalid path to program file: {program_path}."
         
         with open(program_path) as f:
-            lines = f.readlines()
-            print(lines)
+            raw_lines = f.readlines()
+            lines = list(
+                filter(lambda line: line != "", 
+                       map(lambda line: line.strip(), raw_lines)))
+            
+            variables_line = lines[0]
+            program = lines[1:]
+            
+            self.variables = self.parseVariables(variables_line)
+            self.program = Parser.splitLabels(program)
         
 
 
     
     def parseVariables(self, variables_line):
-        variables = set(variables_line.split(" "))
-        pass
+        variables = set(variables_line.strip().split(" "))
+        return variables
 
 
 
--- tests/parser/parser_test.py
+++ tests/parser/parser_test.py
@@ -2,8 +2,6 @@
 SRC_RELATIVE_PATH = "../../src/"
 PARSER_RELATIVE_PATH = SRC_RELATIVE_PATH + 'parser/'
 
-
-
 import sys
 sys.path.insert(1, PARSER_RELATIVE_PATH)
 sys.path.insert(1, "../")
@@ -11,18 +9,34 @@ sys.path.insert(1, "../")
 from parser import Parser
 import test_utils
 
-print(test_utils)
 
 def parser_tester():
-    test_utils.printYellow("Test 1: ...")
+    test_utils.printYellow("Test 1: language_example")
     
     TESTER_PROGRAM_PATH = SRC_RELATIVE_PATH + "../programs/examples/language_example.txt"
     
+    expected_variables_results = {'i', 'j', 'n'}
+    
     try:
         p = Parser(TESTER_PROGRAM_PATH)
+        assert p.variables == expected_variables_results, "The parsed variables are incorrect"
+        assert len(p.program) == 8, "The parsed program is incorrect"
         
+        invalid_lines = list(filter(lambda line: len(line) != 3, p.program.values()))
+        if len(invalid_lines) > 0:
+            test_utils.printError(f"Invalid lines:\n {invalid_lines}")
+            return
+
     except Exception as e:
         test_utils.printError(f'{e}')
+        return
+    print()
+    test_utils.printInfo("Program variables:")
+    test_utils.printInfo(p.variables)
+    print()
+    test_utils.printInfo("Program lines:")
+    test_utils.printInfo(p.program)
+    test_utils.printSuccess("Success!")
     
 
 def run_tests():
